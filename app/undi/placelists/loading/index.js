017c0af42cf47cfad822cc7fc934c19ea7c5f03e
ahmadisyraf <isyrafmagic@gmail.com>
2023-12-06 19:06:11 +0800

    update:  fix skeleton

--- app/undi/placelists/loading/index.js
+++ app/undi/placelists/loading/index.js
@@ -4,15 +4,15 @@ import { Skeleton, Flex, VStack } from "@chakra-ui/react";
 export default function Loading() {
   return (
     <VStack pl={0} align={"start"}>
-      <Skeleton width={"70%"} height={5} />
-      <Skeleton width={"50%"} height={3} />
+      <Skeleton width={"70%"} height={8} />
+      <Skeleton width={"50%"} height={4} />
       <Flex w={"100%"}>
-        <Skeleton width={"10%"} height={3} />
-        <Skeleton width={"10%"} height={3} ml={3} />
+        <Skeleton width={"10%"} height={5} />
+        <Skeleton width={"10%"} height={5} ml={3} />
       </Flex>
       <Flex w={"100%"}>
-        <Skeleton width={"13%"} height={4} />
-        <Skeleton width={"13%"} height={4} ml={3} />
+        <Skeleton width={"13%"} height={6} />
+        <Skeleton width={"13%"} height={6} ml={3} />
       </Flex>
     </VStack>
   );
