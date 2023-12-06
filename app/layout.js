ae1e1d77e44458e9571879e5501cbc54a4fa9f9a
ahmadisyraf <isyrafmagic@gmail.com>
2023-12-06 19:14:27 +0800

    update: add metadata

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,10 @@ import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "UndiLokal - Share Port Best",
-  description: "UndiLokal is your platform to share the best port experiences. Connect with others and discover hidden gems.",
+  description: "UndiLokal adalah platform anda untuk berkongsi pengalaman terbaik di pelabuhan. Berhubung dengan orang lain dan terokai tempat-tempat menarik yang tersembunyi.",
   author: "mistarastapastalaksa", // Replace with your name
   keywords: "port, travel, community, sharing",
-  image: "/undilokal-logo.png", // Replace with the path to your logo or an image representing your site
+  image: "https://images.unsplash.com/photo-1596423735880-5f2a689b903e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with the path to your logo or an image representing your site
   url: "https://www.undilokal.vercel.app", // Replace with your actual website URL
 };
 
